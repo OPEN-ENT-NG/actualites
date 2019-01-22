dca21881edac8a2a426b4259cb379dfc4422859d
Simon Basset <simon.basset@gmail.com>
2019-01-22 09:33:24 +0100

    [Fix] 23152 fix ts compilation

---
+++ .entcore.d.ts
@@ -0,0 +1,26 @@
+export * from "./node_modules/entcore/types/src/ts/http";
+export * from "./node_modules/entcore/types/src/ts/ui";
+export * from "./node_modules/entcore/types/src/ts/notify";
+export * from "./node_modules/entcore/types/src/ts/template";
+export * from "./node_modules/entcore/types/src/ts/lib";
+export * from "./node_modules/entcore/types/src/ts/globals";
+export * from "./node_modules/entcore/types/src/ts/idiom";
+export * from "./node_modules/entcore/types/src/ts/behaviours";
+export * from './node_modules/entcore/types/src/ts/workspace/index';
+export * from './node_modules/entcore/types/src/ts/skin';
+export * from './node_modules/entcore/types/src/ts/editor';
+export * from './node_modules/entcore/types/src/ts/sniplets';
+export * from './node_modules/entcore/types/src/ts/rights';
+export * from './node_modules/entcore/types/src/ts/ng-start';
+export * from './node_modules/entcore/types/src/ts/widget';
+export * from './node_modules/entcore/types/src/ts/me';
+export * from './node_modules/entcore/types/src/ts/libs';
+
+
+/* modelDefinitions typings */
+export { Collection } from "./node_modules/entcore/types/src/ts/modelDefinitions";
+declare class Super {
+    new(): any;
+}
+export class Model extends Super {}
+export const model: any;
