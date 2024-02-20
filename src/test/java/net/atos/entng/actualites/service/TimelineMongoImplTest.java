28fdb04a590b991b0f28d068f4c3d54e9d26b78f
Mattox0 <matbegaaar@gmail.com>
2024-02-20 11:54:50 +0100

    fix(notifs): #CO-1822 add unit tests

--- src/test/java/net/atos/entng/actualites/service/TimelineMongoImplTest.java
+++ src/test/java/net/atos/entng/actualites/service/TimelineMongoImplTest.java
@@ -2,13 +2,11 @@ package net.atos.entng.actualites.service;
 import fr.wseduc.mongodb.MongoDb;
 import io.vertx.core.Handler;
 import io.vertx.core.json.JsonObject;
-import io.vertx.ext.unit.Async;
 import net.atos.entng.actualites.constants.Field;
 import net.atos.entng.actualites.services.impl.TimelineMongoImpl;
 import io.vertx.ext.unit.TestContext;
 import io.vertx.ext.unit.junit.VertxUnitRunner;
 import org.mockito.Mockito;
-import org.entcore.common.user.UserInfos;
 import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
@@ -19,9 +17,6 @@ public class TimelineMongoImplTest {
 
     MongoDb mongo = mock(MongoDb.class);
     private TimelineMongoImpl timelineMongo;
-    private static final String THREAD_ID = "100";
-    private static final String INFO_ID = "123";
-
 
     @Before
     public void setUp(TestContext context) {
@@ -30,8 +25,6 @@ public class TimelineMongoImplTest {
 
     @Test
     public void testGetNotification(TestContext context) {
-        Async async = context.async();
-
         // Expected data
         String expectedCollection = "timeline";
         JsonObject expectedQuery = new JsonObject("{\"params\":{\"resourceUri\":\"/actualites#/view/thread/100/info/123\"}}");
@@ -41,7 +34,6 @@ public class TimelineMongoImplTest {
             JsonObject query = invocation.getArgument(1);
             context.assertEquals(collection, expectedCollection);
             context.assertEquals(query, expectedQuery);
-            async.complete();
             return null;
         }).when(mongo).findOne(Mockito.anyString(), Mockito.any(JsonObject.class), Mockito.any(Handler.class));
 
@@ -50,8 +42,6 @@ public class TimelineMongoImplTest {
 
     @Test
     public void testDeleteNotification(TestContext context) {
-        Async async = context.async();
-
         // Expected data
         String expectedCollection = "timeline";
         JsonObject expectedQuery = new JsonObject("{\"_id\":\"123\"}");
@@ -61,11 +51,9 @@ public class TimelineMongoImplTest {
             JsonObject query = invocation.getArgument(1);
             context.assertEquals(collection, expectedCollection);
             context.assertEquals(query, expectedQuery);
-            async.complete();
             return null;
         }).when(mongo).delete(Mockito.anyString(), Mockito.any(), Mockito.any());
 
-        // get notification ID ?
         timelineMongo.deleteNotification(new JsonObject("{\"_id\":\"123\"}"));
     }
 }
