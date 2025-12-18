cc09542feb847bd24d8f65948d79a4a6b5be20de
Damien Romito Castro <damien.romito-castro@edifice.io>
2025-12-18 12:05:35 +0100

    redirect to right thread when publish

--- frontend/src/features/info-form/hooks/useInfoSharesForm.ts
+++ frontend/src/features/info-form/hooks/useInfoSharesForm.ts
@@ -3,16 +3,17 @@ import { useQueryClient } from '@tanstack/react-query';
 import { useNavigate } from 'react-router-dom';
 import { useI18n } from '~/hooks/useI18n';
 import { InfoId, InfoStatus } from '~/models/info';
+import { ThreadId } from '~/models/thread';
 import { infoQueryKeys, useUpdateInfo } from '~/services/queries';
 
-export function useInfoSharesForm({ infoId }: { infoId: InfoId }) {
+export function useInfoSharesForm() {
   const { t } = useI18n();
   const toast = useToast();
   const { mutate: updateInfoMutate } = useUpdateInfo();
   const navigate = useNavigate();
   const queryClient = useQueryClient();
 
-  const handlePublish = () => {
+  const handlePublish = (infoId: InfoId, threadId: ThreadId) => {
     if (!infoId) {
       throw new Error('infoId is undefined');
     }
@@ -25,11 +26,12 @@ export function useInfoSharesForm({ infoId }: { infoId: InfoId }) {
       },
       {
         onSuccess: () => {
+          //TODO : optimize this invalidation, only invalidate the threads query
           invalidateQueriesWithFirstPage(queryClient, {
             queryKey: infoQueryKeys.all({}),
           });
           toast.success(t('actualites.info.createForm.publishedSuccess'));
-          navigate('/');
+          navigate(`/threads/${threadId}?status=draft`);
         },
       },
     );
