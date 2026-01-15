7f68f12e68a5bb02607e5c237a389518b626851c
jcbe-ode <jean-christophe.benoit@opendigitaleducation.com>
2026-01-15 16:10:00 +0100

    fix: #COCO-5112, wrong counters when 0 stats available

--- frontend/src/services/queries/hooks/useUpdateStatsQueryCache.ts
+++ frontend/src/services/queries/hooks/useUpdateStatsQueryCache.ts
@@ -13,12 +13,24 @@ export const useUpdateStatsQueryCache = () => {
     const queryKey = infoQueryKeys.stats();
     if (!queryClient.getQueryData(queryKey)) return;
     queryClient.setQueryData(queryKey, (oldData: InfosStats): InfosStats => {
-      const targetThread = oldData.threads.find(
+      let threadIndex = oldData.threads.findIndex(
         (thread) => thread.id === threadId,
       );
-      if (!targetThread) return oldData;
+      if (threadIndex < 0) {
+        // Create blank stats if not found, before updating them.
+        threadIndex = oldData.threads.length;
+        oldData.threads.push({
+          id: threadId,
+          status: { DRAFT: 0, PENDING: 0, PUBLISHED: 0, TRASH: 0 },
+          expiredCount: 0,
+          incomingCount: 0,
+        });
+      }
+
+      const targetThread = oldData.threads[threadIndex];
+
       const oldStatusCount = targetThread.status[status] ?? 0;
-      const newStatusCount = oldStatusCount + countDelta;
+      const newStatusCount = Math.max(0, oldStatusCount + countDelta); // countDelta may be negative, but final counter cannot.
 
       const updatedThreads = oldData.threads.map((thread) => {
         if (thread.id !== threadId) {
