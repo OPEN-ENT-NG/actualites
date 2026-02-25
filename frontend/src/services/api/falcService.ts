8f4184369177604f0aee7acfb0a28ab09b567428
jcbe-ode <jean-christophe.benoit@opendigitaleducation.com>
2026-02-25 10:31:30 +0100

    fix: #COCO-5116, use public conf parameters for FALC

--- frontend/src/services/api/falcService.ts
+++ frontend/src/services/api/falcService.ts
@@ -1,6 +1,8 @@
 import { ERROR_CODE, odeServices } from '@edifice.io/client';
 import { baseUrlAPI } from '.';
 
+export type GenAiConf = { falcMinLength: number; falcTimeoutMs: number };
+
 export const createFalcService = () => {
   return {
     async generate(content: string) {
@@ -25,6 +27,25 @@ export const createFalcService = () => {
         }
         return Promise.reject(ERROR_CODE.NOT_INITIALIZED);
       }
+    },
+
+    async getConfiguration(): Promise<GenAiConf> {
+      try {
+        const conf = (await odeServices.conf().getPublicConf('actualites')) as {
+          genai: GenAiConf;
+        };
+        if (
+          conf &&
+          typeof conf.genai === 'object' &&
+          typeof conf.genai.falcMinLength === 'number'
+        ) {
+          return conf.genai;
+        }
+      } catch {
+        // If configuration is not well-defined...
+      }
+      // Fallback to default values
+      return { falcMinLength: 200, falcTimeoutMs: 30000 };
     },
   };
 };
--- frontend/src/services/queries/falc.ts
+++ frontend/src/services/queries/falc.ts
@@ -1,5 +1,7 @@
 import { useMutation } from '@tanstack/react-query';
+import { useEffect, useState } from 'react';
 import { falcService } from '../api';
+import { GenAiConf } from '../api/falcService';
 
 const useGenerateFalc = () =>
   useMutation({
@@ -8,8 +10,18 @@ const useGenerateFalc = () =>
 
 export function useFalc() {
   const mutation = useGenerateFalc();
+  const [configuration, setConfiguration] = useState<GenAiConf>();
+
+  // Load FALC configuration
+  useEffect(() => {
+    (async () => {
+      const configuration = await falcService.getConfiguration();
+      setConfiguration(configuration);
+    })();
+  }, []);
 
   return {
+    configuration,
     isGenerating: mutation.isPending,
     async generate(originalContent: string): Promise<string> {
       const { content } = await mutation.mutateAsync(originalContent);
