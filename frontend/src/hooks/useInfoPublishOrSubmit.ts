067357e1d6337eda8ee91a7f70d06f8eea0efeff
RomuC <romuald.coulaud@gmail.com>
2026-06-22 10:54:03 +0200

    update fix

--- frontend/src/hooks/useInfoPublishOrSubmit.ts
+++ frontend/src/hooks/useInfoPublishOrSubmit.ts
@@ -1,7 +1,7 @@
 import { useToast } from '@edifice.io/react';
 import { useQueryClient } from '@tanstack/react-query';
 import { useNavigate } from 'react-router-dom';
-import { INFO_DATES_RESET_VALUES, INFO_HOURS_DATE_DEFAULT } from '~/features';
+import { INFO_DATES_RESET_VALUES } from '~/features';
 import { useI18n } from '~/hooks/useI18n';
 import { InfoDetails, InfoStatus, UpdateInfoPayload } from '~/models/info';
 import { invalidateThreadQueries, useUpdateInfo } from '~/services/queries';
@@ -26,38 +26,10 @@ export function useInfoPublishOrSubmit() {
     if (!info.publicationDate) {
       payload.publication_date =
         INFO_DATES_RESET_VALUES.publicationDate.toISOString();
-
-      const getExpirationDate = (
-        updateDate: Date,
-        creationDate: Date,
-        expirationDate: string | undefined,
-      ): Date => {
-        if (!expirationDate) {
-          return INFO_DATES_RESET_VALUES.expirationDate;
-        }
-        const updateDateCopy = new Date(updateDate);
-        updateDateCopy.setFullYear(updateDateCopy.getFullYear() + 1);
-        updateDateCopy.setHours(INFO_HOURS_DATE_DEFAULT, 0, 0, 0);
-        const creationDateCopy = new Date(creationDate);
-        creationDateCopy.setFullYear(creationDateCopy.getFullYear() + 1);
-        creationDateCopy.setHours(INFO_HOURS_DATE_DEFAULT, 0, 0, 0);
-
-        const expDate = new Date(expirationDate);
-        // If the expiration date is before the update date or creation date, that mean user set a custom expiration date, we should keep it. Otherwise, we reset it to the default value.
-        if (
-          expDate.getTime() < updateDateCopy.getTime() ||
-          expDate.getTime() < creationDateCopy.getTime()
-        ) {
-          return expDate;
-        }
-        return INFO_DATES_RESET_VALUES.expirationDate;
-      };
-
-      payload.expiration_date = getExpirationDate(
-        new Date(info.modified),
-        new Date(info.created),
-        info.expirationDate,
-      ).toISOString();
+      if (!info.expirationDate) {
+        payload.expiration_date =
+          INFO_DATES_RESET_VALUES.expirationDate.toISOString();
+      }
     }
     updateInfoMutate(
       {
